a5721a7f687fe5c29ff074630f5e64b31e67c7f0
valdir-ti <valdir.ti@gmail.com>
2020-11-11 20:05:05 -0300

    Testando o componente Product

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Product from './components/Product'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Product />
   </React.StrictMode>,
   document.getElementById('root')
 );
