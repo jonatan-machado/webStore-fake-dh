1ab060fe9cbdcc318e78cab1b99f02340b116161
valdir-ti <valdir.ti@gmail.com>
2020-11-11 21:40:47 -0300

    feat: add link to product-details page

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import  Home  from "./pages/Home";
+import  Product  from "./pages/Product";
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/product" exact component={Product} />
       </Switch>
     </BrowserRouter>
   );
