775da60faaea8b06fb15e79057d3548743b04a74
valdir-ti <valdir.ti@gmail.com>
2020-11-11 22:18:58 -0300

    remove error index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import Product from './components/Product'
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Product />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
